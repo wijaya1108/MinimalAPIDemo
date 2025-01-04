3623a44acb5630a2b83dbc58da6eee2ed81492c3
uditha bandara <wijaya.uditha98@gmail.com>
2025-01-04 08:32:19 +0530

    add post endpoint and automapper

--- MinimalAPIDemo/Models/DTO/CouponCreateResponse.cs
+++ MinimalAPIDemo/Models/DTO/CouponCreateResponse.cs
@@ -1,12 +1,11 @@
-﻿namespace MinimalAPIDemo.Models
+﻿namespace MinimalAPIDemo.Models.DTO
 {
-    public class Coupon
+    public class CouponCreateResponse
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int Percent { get; set; }
         public bool IsActive { get; set; }
-        public DateTime? Created { get; set; } = DateTime.UtcNow;
-        public DateTime? LastUpdated { get; set; }
+        public DateTime? Created { get; set; }
     }
 }
