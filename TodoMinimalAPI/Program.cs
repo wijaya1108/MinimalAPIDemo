f0a79da23074b4ded540f73eb534eaecc9c1491c
uditha bandara <wijaya.uditha98@gmail.com>
2025-01-10 22:43:53 +0530

    add and configured ef core db context and ran initial migration

--- TodoMinimalAPI/Program.cs
+++ TodoMinimalAPI/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using TodoMinimalAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//register db context as a service
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection"));
+});
 
 var app = builder.Build();
 
