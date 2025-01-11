8e256d488f2c80efcea373cfab9168b4e8386d40
uditha bandara <wijaya.uditha98@gmail.com>
2025-01-11 21:53:04 +0530

    add get departments endpoint

---
+++ TodoMinimalAPI/Repository/Services/DepartmentRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoMinimalAPI.Data;
+using TodoMinimalAPI.Models;
+using TodoMinimalAPI.Repository.Interface;
+
+namespace TodoMinimalAPI.Repository.Services
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly ILogger<DepartmentRepository> _logger;
+
+        public DepartmentRepository(AppDbContext dbContext, ILogger<DepartmentRepository> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        public async Task<bool> AddDepartment(Department department)
+        {
+            try
+            {
+                var result = await _dbContext.Departments.AddAsync(department);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Cannot insert the department");
+                return false;
+            }
+        }
+
+        public Task<bool> DeleteDepartment(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Department> GetDepartmentById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Department>> GetDepartments()
+        {
+            var departments = await _dbContext.Departments.ToListAsync();
+            _logger.LogInformation("department retrievel successfull");
+            return departments;
+        }
+
+        public Task<Department> UpdateDepartment(Department department)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
