75eaf3e4f050ac08c4810d1586c670488eccd4ab
uditha bandara <wijaya.uditha98@gmail.com>
2025-01-11 09:22:38 +0530

    add repositories for db operations

---
+++ TodoMinimalAPI/Repository/Interface/IDepartmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using TodoMinimalAPI.Models;
+
+namespace TodoMinimalAPI.Repository.Interface
+{
+    public interface IDepartmentRepository
+    {
+        Task<IEnumerable<Department>> GetDepartments();
+        Task<Department> GetDepartmentById(Guid id);
+        Task<bool> AddDepartment(Department department);
+        Task<Department> UpdateDepartment(Department department);
+        Task<bool> DeleteDepartment(Guid id);
+    }
+}
